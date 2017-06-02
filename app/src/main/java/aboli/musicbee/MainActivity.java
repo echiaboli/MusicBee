da03ae90a5d3ea73432bf0c4a7565b577fd96d55
caleblj <joh16002@byui.edu>
2017-06-02 13:39:17 -0600

    my test

--- app/src/main/java/aboli/musicbee/MainActivity.java
+++ app/src/main/java/aboli/musicbee/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         test; //this makes no sense
+        test2;
     }
 }

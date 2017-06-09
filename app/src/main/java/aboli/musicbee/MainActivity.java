215d8df7895241bb9891517382e8795de0b7994d
echiaboli <ech11004@byui.edu>
2017-06-09 12:52:41 -0600

    Added a JUnit Test case

--- app/src/main/java/aboli/musicbee/MainActivity.java
+++ app/src/main/java/aboli/musicbee/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test; //this makes no sense
-        test2;
+        //test; //this makes no sense
+        //test2;
     }
 }
--- app/src/test/java/aboli/musicbee/ExampleUnitTest.java
+++ app/src/test/java/aboli/musicbee/ExampleUnitTest.java
@@ -4,14 +4,35 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void doSubtract() throws Exception {
+        assertEquals(1, 3 - 2 );
+        ;
+    }
+
+@Test
+    public void decideSkill() throws Exception {
+    int skill = 0;
+    assertEquals(1, skill == 1);
 }
+
+
+
+
+
+}
+

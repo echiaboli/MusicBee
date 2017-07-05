112ef9b35daf0ea2b4b2ec681bca3ac7339be82e
Jesse <xalmia@gmail.com>
2017-07-05 13:40:04 -0600

    Changed Insruction_Page for testing of EasyGame

--- app/src/main/java/aboli/musicbee/Instruction_Page.java
+++ app/src/main/java/aboli/musicbee/Instruction_Page.java
@@ -1,12 +1,19 @@
 package aboli.musicbee;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class Instruction_Page extends AppCompatActivity {
 
+    //temporary function to override screen and start easy activity for testing
+    protected void clickButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), easyGame.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

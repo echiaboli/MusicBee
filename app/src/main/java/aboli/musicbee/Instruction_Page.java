f411c045299dc57e3ea182082f4a3e56c7366a23
caleblj <joh16002@byui.edu>
2017-07-10 12:16:16 -0600

    Instruction page

--- app/src/main/java/aboli/musicbee/Instruction_Page.java
+++ app/src/main/java/aboli/musicbee/Instruction_Page.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Instruction_Page extends AppCompatActivity {
@@ -27,23 +28,17 @@ public class Instruction_Page extends AppCompatActivity {
     public void IntroMessage(String difficulty) {
         if (difficulty == "Easy") {
             Context context = getApplicationContext();
-            CharSequence text = "Try and press the correct keys on the keyboard according to the words displayed above! After you enter the notes hit submit. If the answers is correct" +
-                    " you will gain 5 seconds. If you guess wrong it's minus 5 seconds! Try to get as many correct before the time runs out!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+            TextView t = (TextView) findViewById(R.id.displayDiff);
+            t.setText("Try and press the correct keys on the keyboard according to the words displayed above! After you enter the notes hit submit. If the answers is correct" +
+                    " you will gain 5 seconds. If you guess wrong it's minus 5 seconds! Try to get as many correct before the time runs out!");
 
         } else if(difficulty == "Intermediate") {
 
         } else if(difficulty == "Hard") {
             Context context = getApplicationContext();
-            CharSequence text = "The staff will appear with notes on it. Try and guess the word above with the letters below. After you enter the letters hit submit. If the answers is correct" +
-                    " you will gain 5 seconds. If you guess wrong it's minus 5 seconds! Try to get as many correct before the time runs out!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+            TextView t = (TextView) findViewById(R.id.displayDiff);
+            t.setText("The staff will appear with notes on it. Try and guess the word above with the letters below. After you enter the letters hit submit. If the answers is correct" +
+                    " you will gain 5 seconds. If you guess wrong it's minus 5 seconds! Try to get as many correct before the time runs out!");
 
         }
     }
